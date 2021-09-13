86d358845350ed67fc17b656a135278127fcdf8e
bart <holek10@users.noreply.github.com>
2021-09-14 11:27:18 +1200

    Add additional resources

--- proxy-pac.js
+++ proxy-pac.js
@@ -8,7 +8,9 @@ function FindProxyForURL(url, host) {
         dnsDomainIs(host, "digital-integration-prelive.skyone.co.nz") ||
         dnsDomainIs(host, "delivery-dev.skyone.co.nz") ||
         dnsDomainIs(host, "delivery-sit.skyone.co.nz") ||
-        dnsDomainIs(host, "test-goskygo.skygo.co.nz"))
+        dnsDomainIs(host, "test-goskygo.skygo.co.nz") ||
+        dnsDomainIs(host, "dev.sky.co.nz") ||
+        dnsDomainIs(host, "prelive.sky.co.nz"))
         return "PROXY proxy.skytv.co.nz:8080"; // (IP:port)
 
     return "DIRECT";
