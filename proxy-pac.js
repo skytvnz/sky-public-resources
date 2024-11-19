2d2bd2ad80d6a968e640c46ec35d35639282d63a
Adrian Baker <62906763+adrian-skybaker@users.noreply.github.com>
2024-11-19 17:15:26 +1300

    Update proxy-pac.js

--- proxy-pac.js
+++ proxy-pac.js
@@ -18,6 +18,7 @@ function FindProxyForURL(url, host) {
         dnsDomainIs(host, "dev.sky.co.nz") ||
         dnsDomainIs(host, "skyprelive.co.nz") ||
         dnsDomainIs(host, "172.26.121.101") ||
+        dnsDomainIs(host, "172.26.34.69") ||
         dnsDomainIs(host, "prelive.sky.co.nz"))
         return "PROXY proxy.skytv.co.nz:8080"; // (IP:port)
 
